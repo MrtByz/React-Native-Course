c7c7250c872d1dbf31b280b4ebd1407e65402ed9
MrtByz <mbeyaz@gtu.edu.tr>
2022-06-30 16:55:49 +0300

    type check and conversion

---
+++ Day 2/JS_Course/typecheck.js
@@ -0,0 +1,23 @@
+let price = 111
+console.log("price ", typeof(price))
+
+let stringPrice = "111"
+console.log("string price ", typeof(stringPrice))
+
+let hasPassword = true
+console.log("Password ", typeof(hasPassword))
+
+
+// Stringi sayıya çevirme
+let num1 = "11px"
+console.log("num1: ", parseInt(num1))
+// px11 olsa NaN dönerdi
+
+// Stringi floata çevirme
+let num2 = "11px"
+console.log("num2: ", parseFloat(num2))
+
+// Sayıyı stringe çevirme
+let num3 = 11
+console.log("num3: ", num3.toString())
+
