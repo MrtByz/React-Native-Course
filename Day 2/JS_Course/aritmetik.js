ddd59a0a981941a5ae561ecb2b9aad14ff842236
MrtByz <mbeyaz@gtu.edu.tr>
2022-06-30 15:38:04 +0300

    JavaScript training

---
+++ Day 2/JS_Course/aritmetik.js
@@ -0,0 +1,31 @@
+var x = 15;
+var y = 3;
+
+
+var toplam = x + y
+console.log(toplam)
+
+var fark = x - y
+console.log(fark)
+
+var çarp = x * y
+console.log(çarp)
+
+var böl = x / y
+console.log(böl)
+
+var kuvvet = x ** y
+console.log(kuvvet)
+
+var mod = x % y
+console.log(mod)
+
+x++
+console.log(x)
+
+y--
+console.log(y)
+
+
+
+
---
+++ Day 2/JS_Course/degisken.js
@@ -0,0 +1,17 @@
+// var ile değişken tanımlama
+
+var serverNameVar = "ReactDeneme - Var";
+
+console.log(serverNameVar)
+
+// let ile değişken tanımlama
+
+let serverNameLet = "ReactDeneme - Let";
+
+console.log(serverNameLet)
+
+// const ile değişken tanımlama
+
+const serverNameCon = "ReactDeneme - Const";
+
+console.log(serverNameCon)
